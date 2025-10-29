5b278240dcb67fb016291d2119f5a39494a0f437
Slurmlord <slurmlord@outlook.com>
2025-10-29 21:52:00 +0100

    Avoid chopping multibyte char in half

--- GeneralsMD/Code/GameEngine/Source/GameNetwork/GameInfo.cpp
+++ GeneralsMD/Code/GameEngine/Source/GameNetwork/GameInfo.cpp
@@ -978,6 +978,11 @@ static Bool TruncatePlayerNames(AsciiStringVec& playerNames, Int truncateAmount)
 			truncateNameAmount = std::max(truncateAmount, truncateNameAmount);
 		}
 
+		// as the name is UTF-8, make sure we don't truncate part of a multibyte character
+		while ((playerNames[lengthIndex[i].Index][lengthIndex[i].Length - truncateNameAmount - 1] & 0xC0) == 0x80) {
+			++truncateNameAmount; // move back to the start of the multibyte character
+		}
+
 		playerNames[lengthIndex[i].Index].truncateBy(truncateNameAmount);
 		truncateAmount -= truncateNameAmount;
 	}
